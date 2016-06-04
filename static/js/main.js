cd150a6220941a3cffeec41305925b9d017ae304
Bart Enkelaar <benkelaar@bol.com>
2016-06-04 21:50:32 +0200

    Add adjustable final column

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,21 @@
 
 var THC = {};
 
+THC.list = [
+  'green-up',
+  'green-same',
+  'green-down',
+  'yellow-up',
+  'yellow-same',
+  'yellow-down',
+  'orange-up',
+  'orange-same',
+  'orange-down',
+  'red-up',
+  'red-same',
+  'red-down',
+];
+
 THC.getParameterByName = function (name) {
   name = name.replace(/[\[\]]/g, "\\$&");
     var url = window.location.href,
@@ -12,17 +27,44 @@ THC.getParameterByName = function (name) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 };
 
-THC.populateTable = function (data, team) {
-  var table = $('#checks').empty()
+THC.activateModifiables = function () {
+  function clickEvent(e) {
+    var td   = $(e.currentTarget),
+        from = td.data().lvl,
+        now  = THC.list.indexOf(from),
+        down = e.which > 1 || e.altKey || e.ctrlKey || e.metaKey || e.shiftKey,
+        last = THC.list.length - 1,
+        to   = down ? (now == last ? 0 : now + 1) : (now == 0 ? last : now - 1),
+        next = THC.list[to];
+
+    td.data('lvl', next);
+    td.find('img').attr('src', 'images/'+next+'.png');
+    return false;
+  }
+
+  $('.modifiable')
+    .click(clickEvent)
+    .contextmenu(clickEvent);
+};
+
+THC.populateTable = function (data) {
+  if (!data || !data.length) {
+    console.log('empty data returned from server');
+    return;
+  }
+  var team = THC.team,
+      table = $('#checks').empty()
     .append($('<thead><tr><th>'+team+'</th></tr></thead><tbody />'));
 
   function showValue(name, value, column) {
-    var id  = '#' + name,
-        row = table.find(id),
-        img = value.level + '-' + value.direction,
-        row = row.length ? row : table.find('tbody')
-            .append($('<tr id="'+name+'"><td>'+name+'</td></tr>')).find(id),
-        cur = row.find('td').length
+    var id      = '#' + name,
+        row     = table.find(id),
+        imgName = value.level + '-' + value.direction,
+        row     = row.length ? row :
+            table.find('tbody')
+                .append($('<tr id="'+name+'"><td>'+name+'</td></tr>')).find(id),
+        cur     = row.find('td').length,
+        img     = $('<td><img src="images/'+imgName+'.png"></td>');
 
     // Add missing columns if previous dates did not have a check
     if (cur != column) {
@@ -30,7 +72,8 @@ THC.populateTable = function (data, team) {
         row.append($('<td />'));
       }
     }
-    row.append($('<td><img src="images/'+img+'.png"></td>'));
+    img.data('lvl', imgName);
+    row.append(img);
   }
 
   function showCheck(check) {
@@ -44,19 +87,21 @@ THC.populateTable = function (data, team) {
   }
 
   data.forEach(showCheck);
+  showCheck(data[data.length - 1]);
+  table.find('th:last-child').html('New');
+  table.find('td:last-child').addClass('modifiable')
+
+  THC.activateModifiables()
 };
 
-THC.loadTable = function (team) {
-  $.ajax('http://localhost:3000/v1/checks?team=' + team, {
-    dataType: 'json',
-    success: function (data) {
-      THC.populateTable(data, team);
-    }
+THC.loadTable = function () {
+  $.ajax('http://localhost:3000/v1/checks?team=' + THC.team, {
+    success: THC.populateTable
   });
 };
 
 $(document).ready(function () {
   THC.team = THC.getParameterByName('team');
   if (!THC.team) THC.team = 'Team15b';
-  THC.loadTable(THC.team);
+  THC.loadTable();
 });
