e8511f8f5baa5bb6031a3fb45226fa4ea60af59d
Bart Enkelaar <benkelaar@bol.com>
2016-06-04 09:46:57 +0200

    Populate checks table from backend

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,59 @@
 "use strict";
+
+var THC = {};
+
+THC.getParameterByName = function (name) {
+  name = name.replace(/[\[\]]/g, "\\$&");
+    var url = window.location.href,
+        regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+
+THC.populateTable = function (data, team) {
+  var table = $('#checks').empty()
+    .append($('<thead><tr><th>'+team+'</th></tr></thead><tbody />'));
+
+  function showValue(name, value, column) {
+    var id  = '#' + name,
+        row = table.find(id),
+        img = value.level + '-' + value.direction;
+
+    row = row.length ? row : table.find('tbody')
+        .append($('<tr id="'+name+'"><td>'+name+'</td></tr>')).find(id);
+
+    if (row.find('td').length != (column - 1)) {
+      
+    }
+    row.append($('<td><img src="images/'+img+'.png"></td>'));
+  }
+
+  function showCheck(check) {
+    var time   = new Date(check.time),
+        day    = time.getDate() + '-' + (time.getMonth() + 1),
+        column = table.find('thead tr').append($('<th>'+day+'</th>'))
+                      .find('th').length;
+    for (var key in check.health) {
+      showValue(key, check.health[key], column);
+    }
+  }
+
+  data.forEach(showCheck);
+};
+
+THC.loadTable = function (team) {
+  $.ajax('http://localhost:3000/v1/checks?team=' + team, {
+    dataType: 'json',
+    success: function (data) {
+      THC.populateTable(data, team);
+    }
+  });
+};
+
+$(document).ready(function () {
+  THC.team = THC.getParameterByName('team');
+  if (!THC.team) THC.team = 'Team15b';
+  THC.loadTable(THC.team);
+});
